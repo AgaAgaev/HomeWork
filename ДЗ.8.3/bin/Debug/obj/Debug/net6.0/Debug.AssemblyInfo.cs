9dd7ce02cecebfc28cdee6bc9dc49b91fb0a70e6
agaagaev <agaagaev.96@gmail.com>
2023-07-14 01:30:11 +0500

    Исправление2

--- ДЗ.8.3/bin/Debug/obj/Debug/net6.0/Debug.AssemblyInfo.cs
+++ ДЗ.8.3/bin/Debug/obj/Debug/net6.0/Debug.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ДЗ.8.3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Debug")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ДЗ.8.3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ДЗ.8.3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Debug")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Debug")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
